1fd42b3430837ae0a0eab6afdb4ca7b3cf5f3539
MarcoBarrientos <130726817+MarcoBarrientos@users.noreply.github.com>
2026-03-06 23:39:02 -0600

    Add files via upload

---
+++ Desafio 2 parte 2/Desafio 2 parte 2/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static int puntajeRecord = 0;
+    static string jugadorRecord = "";
+    static void VerificarRecord(int puntaje, string nombreJugador)
+    {
+        if (puntaje > puntajeRecord)
+        {
+            puntajeRecord = puntaje;
+            jugadorRecord = nombreJugador;
+
+            Console.WriteLine("La nueva puntuación más alta es " + puntajeRecord);
+            Console.WriteLine("La puntuación más alta fue lograda por " + jugadorRecord);
+        }
+        else
+        {
+            Console.WriteLine("La puntuación más alta de " + puntajeRecord +
+            " no se ha podido superar, y aún está en manos de " + jugadorRecord);
+        }
+    }
+
+    static void Main()
+    {
+        VerificarRecord(100, "Dayani");
+        VerificarRecord(50, "Glendy");
+        VerificarRecord(150, "Yovi");
+    }
+}
